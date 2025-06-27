11cdf271a7f99a4c657c29cd12a97f0a7f92011a
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-06-27 17:17:54 +0700

    Update ahjhj.lua

--- ahjhj.lua
+++ ahjhj.lua
@@ -1,30 +1,10 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
-local window = library:Window("TranVanBaoDz")
+local window = library:Window("j")
 
-function SkillDown(va)
-    game:GetService("VirtualInputManager"):SendKeyEvent(true, va, false, game)
-end
-
-function SkillUp(va)
-    game:GetService("VirtualInputManager"):SendKeyEvent(false, va, false, game)
-end
-
-local function useSkill(key)
-    SkillDown(key)
-    wait(0.1)
-    SkillUp(key)
-end
-
-window:Button("console", function()
-    useSkill("F9")
-end)
-
--- Copy CFrame Button
 window:Button("CopyCframe", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/meobeo8/SupportBuLon/main/CoppyCframe"))()
 end)
 
--- Separate Buttons for Infinite Yield and Fly
 window:Button("Infinite Yield", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
@@ -33,17 +13,14 @@ window:Button("FlyBuLon", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/SupportBuLon/main/FlyBuLon"))()
 end)
 
--- Dex Vippro Button
 window:Button("Dex Vippro", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/DEX-Explorer/refs/heads/main/Mobile.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DexPlusPlus/refs/heads/master/out.lua"))()
 end)
 
--- RemoteSpy Button
 window:Button("RemoteSpy", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/SimpleSpy/refs/heads/main/Mobile.lua"))()
 end)
 
--- RemoteSpy 1 Button
 window:Button("RemoteSpy 1", function()
     local owner = "Upbolt"
     local branch = "revision"
